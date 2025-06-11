4b279c01f0849eabcb32c7f757111075ba8be505
Nasr-Eddine Farid <nafarid@c2r1p1.1337.ma>
2025-06-11 19:19:10 +0100

    pars env is finish

--- executing/bultins/env_cmd.c
+++ executing/bultins/env_cmd.c
@@ -6,7 +6,7 @@
 /*   By: nafarid <nafarid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 13:56:24 by nafarid           #+#    #+#             */
-/*   Updated: 2025/06/11 15:07:31 by nafarid          ###   ########.fr       */
+/*   Updated: 2025/06/11 19:17:39 by nafarid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,43 @@ void print_env(char **envp)
         i++;
     }
 }
-
+int pars_env(char **av, char **envp)
+{
+    int i;
+    int bol;
+    
+    i = 1;
+    bol = 0;
+    while(av[i])
+    {
+        if(ft_strcmp(av[i], "env") != 0)
+        {
+            bol = 1;
+            break;
+        }
+        i++;
+    }
+    if(bol)
+    {
+        printf("Minishell: env: %s: No such file or directory\n", av[i]);
+        return 127;
+    }
+    else
+        print_env(envp);
+    return 0;
+}
 int env_cmd(char **av, char **envp)
 {
     int len_args;
-
     len_args = cnt_string(av);
     if(len_args == 1)
     {
         print_env(envp);
         return 0;
+    }
+    else
+    {
+        return(pars_env(av, envp));
     }
     return 0;
 

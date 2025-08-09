82f6adcfc89b01ab31056842a1cca257ae3ded02
Your Name <Houssam142@example.com>
2025-08-09 18:02:54 +0100

    fakjwf

--- minishell/lstadd_front.c
+++ minishell/lstadd_front.c
@@ -34,19 +34,6 @@ void	check_if_should_split(t_token *toks)
 	}
 }
 
-char	*remove_outer_quotes(char *s)
-{
-	size_t	len;
-
-	if (!s)
-		return (NULL);
-	len = ft_strlen(s);
-	if (len >= 2 && ((s[0] == '"' && s[len - 1] == '"') || (s[0] == '\''
-				&& s[len - 1] == '\'')))
-		return (ft_substr(s, 1, len - 2));
-	return (ft_strdup(s));
-}
-
 void	ft_lstadd_front(t_cmd_exec **lst, t_cmd_exec *new)
 {
 	if (lst && new)
